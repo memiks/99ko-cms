f085722a4349c6820ffe9fbbffc8ee3f045906d6
Frédéric K <stradfred@gmail.com>
2012-05-12 20:52:58 +0200

    Commit Stable version 1.2.1

---
+++ admin/js/plugin-config.js
@@ -0,0 +1,15 @@
+function openPluginConfig() {
+	$('#pluginConfig').show(200);
+	$('#pluginConfigButton').unbind('click', openPluginConfig);
+	$('#pluginConfigButton').click(closePluginConfig);
+}
+
+function closePluginConfig() {
+	$('#pluginConfig').hide(200);
+	$('#pluginConfigButton').unbind('click', closePluginConfig);
+	$('#pluginConfigButton').click(openPluginConfig);
+}
+
+$(document).ready(function () {
+	$('#pluginConfigButton').click(openPluginConfig);
+});

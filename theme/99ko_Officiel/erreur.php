1b9ad346c1b39311b7a33fbb667137328bf513a6
Fred89 <stradfred@gmail.com>
2012-12-22 09:06:59 +0100

    Retrait des plugins et thèmes  non d'origine

--- theme/99ko_Officiel/erreur.php
+++
@@ -1,6 +0,0 @@
-<?php include('header.php'); ?>
-	<div id="Titre_logo">
-		<img src="<?php echo $data['siteUrl']; ?>theme/<?php echo $data['theme']; ?>/images/featured.png" alt="featured section" />
-	</div><!--/Titre_logo-->
-<p id="msg">La page demandée n'a pas été trouvée.</p>
-<?php include('footer.php'); ?>

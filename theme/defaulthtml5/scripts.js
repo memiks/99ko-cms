95aafb147937ae4f6a508855eb084591d5628376
jonathan84 <j.coulet@gmail.com>
2013-10-09 21:20:34 +0200

    del

--- theme/defaulthtml5/scripts.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-	$('body').append('<a id="top" href="javascript:">Haut de page</a>');
-        $(window).scroll(function(){
-	});
-	$('#top').click(function(){
-	    $('html,body').animate({scrollTop:0}, 'fast');
-	});
-});
